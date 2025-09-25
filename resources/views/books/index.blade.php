05b5e5e98dfcdc504a6f4d1a38d459134a070d3c
Adika Muhammad <adikamuhammad1998@gmail.com>
2025-09-25 16:38:01 +0700

    filter popular and highest rated

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="mb-10 text-2xl">Books</h1>
+
+    <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+        <input class="input h-10" type="text" name="title" placeholder="Search by title" value="{{ request('title') }}">
+        <input type="hidden" name="filter" value="{{ request('filter') }}" />
+        <button class="btn h-10" type="submit">Search</button>
+        <a class="btn h-10" href="{{ route('books.index') }}">Clear</a>
+    </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'Latest',
+                'popular_last_month' => 'Popular Last Month',
+                'popular_last_6months' => 'Popular Last 6 Months',
+                'highest_rated_last_month' => 'Highest Rated Last Month',
+                'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+            ];
+        @endphp
+
+        @foreach ($filters as $key => $label)
+            <a class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}"
+                href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}">{{ $label }}</a>
+        @endforeach
+    </div>
+
+    <ul>
+        @forelse ($books as $book)
+            <li class="mb-4">
+                <div class="book-item">
+                    <div class="flex flex-wrap items-center justify-between">
+                        <div class="w-full flex-grow sm:w-auto">
+                            <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+                            <span class="book-author">by {{ $book->author }}</span>
+                            <span class="text-xs text-slate-600">
+                                {{ \Carbon\Carbon::parse($book->created_at)->format('d-m-Y') }}</span>
+                        </div>
+                        <div>
+                            <div class="book-rating">
+                                {{ number_format($book->reviews_avg_rating, 1) }}
+                            </div>
+                            <div class="book-review-count">
+                                out of {{ $book->reviews_count }}
+                                {{ Str::plural('review', $book->reviews_count) }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        @empty
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text">No books found</p>
+                    <a href="{{ route('books.index') }}" class="reset-link">Reset criteria</a>
+                </div>
+            </li>
+        @endforelse
+    </ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/',
+    function () {
+        return view('welcome');
+    }
+);
+
+Route::resource(
+    'books',
+    BookController::class
+);
